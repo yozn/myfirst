41ca695938f5b7736b714961382d70ca99ae37e9
Yozn <a757777@gmail.com>
2017-09-13 22:48:56 +0800

    5

--- 123.py
+++ 123.py
@@ -25,17 +25,19 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+	print(app.logger.info)
 
     # handle webhook body
     try:
         handler.handle(body, signature)
+		print('get handle')
     except InvalidSignatureError:
         abort(400)
 
     return 'OK'
 
 
-#@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print('hello!2')
     line_bot_api.reply_message(
