e35b3e30459923c5e1174c63b33255dcb4e0aab6
Yozn <a757777@gmail.com>
2017-09-13 22:09:30 +0800

    ss

--- 123.py
+++ 123.py
@@ -36,10 +36,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    #line_bot_api.reply_message(
-     #   event.reply_token,
-      #  TextSendMessage(text=event.message.text))
-	print("hiiiii")
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
 
 if __name__ == "__main__":
     app.run()
