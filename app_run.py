aa4946b42ff966d1990b37b6c57c9998370bc515
Yozn <a757777@gmail.com>
2017-09-13 20:11:50 +0800

    update

--- app_run.py
+++ app_run.py
@@ -1,41 +1,45 @@
-from flask import *
-from datetime import datetime
-from dbModel import *
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
 
-
-@app.route('/')
-@app.route('/index')
-def index():
-    data = "Deploying a Flask App To Heroku"
-    data_UserData = UserData.query.all()
-    history_dic = {}
-    history_list = []
-    for _data in data_UserData:
-        history_dic['Name'] = _data.Name
-        history_dic['Id'] = _data.Id
-        history_dic['Description'] = _data.Description
-        history_dic['CreateDate'] = _data.CreateDate.strftime('%Y/%m/%d %H:%M:%S')
-        history_list.append(history_dic)
-        history_dic = {}
-    return render_template('index.html', **locals())
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 
-@app.route('/API/add_data', methods=['POST'])
-def add_data():
-    name = request.form['name']
-    description = request.form['description']
-    if name != "" and description != "":
-        add_data = UserData(
-            Name=name,
-            Description=description,
-            CreateDate=datetime.now()
-        )
-        db.session.add(add_data)
-        db.session.commit()
-    return redirect('index')
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()
