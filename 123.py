af8e8e9f9f636d2ac55c35654ded61bd6758ce3d
Yozn <a757777@gmail.com>
2017-09-14 08:04:29 +0800

    1

--- 123.py
+++ 123.py
@@ -14,18 +14,20 @@ app = Flask(__name__)
 
 line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
 handler = WebhookHandler('YOUR_CHANNEL_SECRET')
-parser = linebot.WebhookParser('YOUR_CHANNEL_SECRET')
+# parser = linebot.WebhookParser('YOUR_CHANNEL_SECRET')
 
 @app.route("/callback", methods=['POST'])
 def callback():
 
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+    print(signature)
 
     # get request body as text
     body = request.get_data(as_text=True)
-    events = parser.parse(body, signature)
-    print(events)
+    print(body)
+    # events = parser.parse(body, signature)
+    # print(events)
     json_line = request.get_json()
     tok=json_line['events'][0]['replyToken']
     textt=json_line['events'][0]['message']['text']
