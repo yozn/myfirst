5316c2d574f508a06cd3140f761b8e403eb70718
yozn <a757777@gmail.com>
2017-09-14 15:07:46 +0800

    3

--- 123.py
+++ 123.py
@@ -72,6 +72,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print('hello!2')
+    print(event.reply_token)
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
